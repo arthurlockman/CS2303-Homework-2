952212760a5fe17a0085b9ee47ff75a99a4c2497
Arthur Lockman <hello@rthr.me>
2015-01-24 18:22:42 -0500

    Parts 5-7 done, only part 4 remains.

---
+++ at2.c
@@ -0,0 +1,20 @@
+#include "print_arrays.h"
+#include <stdlib.h>
+
+/** Main program for demonstrating arrays. It fills them and prints them.
+ * @return 0, Indicating success.
+ */
+
+int main(int argc, const char* argv[]) {
+  int i; // Loop counter
+  int a[argc-1]; // Sample array for demonstration
+
+  // Fill the array with consecutive integers
+  for (i = 0; i < argc -1; i++) a[i] = atoi(argv[i+1]);
+
+  // Now print it out
+  print_int_array(a, argc - 1);
+
+  return 0; // Success!
+}
+
--- makefile
+++ makefile
@@ -1,14 +1,19 @@
-all: at
+all: at at2
 
 at: at.o print_arrays.o
-	gcc at.o print_arrays.o -o at
+	gcc at.o print_arrays.o -o at -g
+
+at2: at2.o print_arrays.o
+	gcc at2.o print_arrays.o -o at2 -g
 
 at.o: at.c print_arrays.h
-	gcc -c at.c
+	gcc -g -c at.c
+at2.o: at2.c print_arrays.h
+	gcc -g -c at2.c
 
 print_arrays.o: print_arrays.c print_arrays.h
-	gcc -c print_arrays.c
+	gcc -g -c print_arrays.c
 
 clean:
-	rm -f *.o at
+	rm -f *.o at at2
 
