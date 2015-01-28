0d5431aef20d97b75f76f033c3a0f3f715875a0b
Arthur Lockman <hello@rthr.me>
2015-01-28 11:35:51 -0500

    Updated at2.

--- at2.c
+++ at2.c
@@ -10,25 +10,31 @@ int ingest_array(const char* args[], size_t size, int dest[]);
 int main(int argc, const char* argv[])
 {
     int i; // Loop counter
-    int a[argc - 1]; 
-    
+    int a[argc - 1];
+
+    if (argc < 2)
+    {
+        printf("You need to enter a number on the command line!\n");
+        return 1;
+    }
+
     // Fill the array with consecutive integers
     for (i = 0; i < argc - 1; i++) a[i] = atoi(argv[i + 1]);
 
     // Now print it out
     print_int_array(a, argc - 1);
-    
-    int b[argc - 1];
-    ingest_array(argv, argc - 1, b); 
-    print_int_array(b, argc - 1);
+
+    /* int b[argc - 1]; */
+    /* ingest_array(argv, argc - 1, b); */
+    /* print_int_array(b, argc - 1); */
 
     return 0; // Success!
 }
 
-/** 
+/**
  * @brief Ingests an array of integers from the command line. It takes
  * in an array from argv and ingests it into an input array.
- * 
+ *
  * @param args[] The array created in argv in the main loop.
  * @param size The size of the input array.
  * @param dest[] The array to put the new values in.
@@ -40,7 +46,7 @@ int ingest_array(const char* args[], size_t size, int dest[])
     int i;
     for (i = 1; i <= size; i++)
     {
-        dest[i-1] = atoi(args[i]);
+        dest[i - 1] = atoi(args[i]);
     }
     return 0;
 }
