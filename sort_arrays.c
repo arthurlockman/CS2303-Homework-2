203d41e1708ce076cb2ce3109752ff2158e0ead4
Arthur Lockman <hello@rthr.me>
2015-01-30 10:18:50 -0500

    DEAL WITH IT

--- sort_arrays.c
+++ sort_arrays.c
@@ -12,17 +12,20 @@
 void bubble_sort(int unsorted[], size_t size)
 {
     int i, j;
+    int k = 0;
     for (j = size; j > 0; j--)
     {
         for (i = 1; i < j; i++)
         {
             if (unsorted[i] < unsorted[i - 1])
             {
+                k = 1;
                 int tmp = unsorted[i - 1];
                 unsorted[i - 1] = unsorted[i];
                 unsorted[i] = tmp;
             }
         }
+        if (k == 0) break; //break out if already sorted (no swaps)
     }
 }
 
